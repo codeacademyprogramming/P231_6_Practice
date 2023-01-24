e9fec3e056ecf94715e50a5f4c16efdc2175cef3
eaghayev <eaghayevv@gmail.com>
2023-01-24 17:54:00 +0400

    project added

---
+++ 24011021/Homework/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+
+            AddTen(ref num);
+
+            Console.WriteLine(num);
+
+            var str = "Sabahin xeyir Baki. Baki gozel seherdir. Bakiya gelin";
+
+            var arr = str.Split(' ');
+           var newStr = string.Join('+', arr);
+            Console.WriteLine(newStr);
+
+
+            str.Substring(str.LastIndexOf(' ')+1);
+            str.Substring(9, str.LastIndexOf(' ') - 9);
+
+            str.EndsWith("Baki");
+
+            str.ToLower().Contains("sabah");
+
+            var numbers = new int[] { 34, 45, 1, 43, 23 };
+
+            Add(ref numbers, 5);
+            Add(ref numbers, 15);
+            Add(ref numbers, 25);
+
+            Array.Resize(ref numbers, 3);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+
+
+
+        }
+
+        static void Resize(ref int[] arr,int newSize)
+        {
+            int[] newArr = new int[newSize];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = arr[i];
+            }
+
+            arr=newArr;
+        }
+
+        static void Add(ref int[] numbers,int num)
+        {
+            int[] newArr = new int[numbers.Length + 1];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                newArr[i] = numbers[i];
+            }
+
+            newArr[newArr.Length - 1] = num;
+
+            numbers = newArr;
+        }
+
+        static void AddTen(ref int number)
+        {
+            number = number + 10;
+        }
+
+    }
+}
---
+++ 24011021/Tasks/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsName("Hikmet"));
+
+            string[] fullnames = { "Hikmet    Abbas", "Tofiq Qulamov", "Nermin Abbasova", "Hakim Abdullayev" };
+            var names = MakeNames(fullnames);
+            //{"Hikmet", "Tofiq","Nermin","Hakim"}
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            string[] arr = { "alma", "armud", "Hikmet", "alma", "Abbas","Hikmet" };
+
+            var newArr = MakeUniqueArr(arr);
+
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                Console.WriteLine(newArr[i]);
+            }
+            //{"alma","armud","Hikmet","Abbas"}
+
+            //Verilmis yazini bas herfi boyuk digerleri kicik halda qaytaran metod
+            string name = "HIKmeT";
+            Capitalize(ref name);//Hikmet
+            Console.WriteLine(name);
+
+            string nameInput;
+            do
+            {
+                Console.WriteLine("Adinizi daxil edin:");
+                nameInput = Console.ReadLine();
+
+            } while (IsName(nameInput)==false);
+
+
+            //cosole-da email deyeri istenilsin. Eger deyerde @ xarakteri yoxdursa yeniden istenilsin
+            string email;
+            do
+            {
+                Console.WriteLine("Emailinizi daxil edin:");
+                email = Console.ReadLine();
+
+            } while (!email.Contains('@'));
+
+            Console.WriteLine(GetDomain(email));
+
+        }
+
+        static string GetDomain(string email)
+        {
+            //hiko@gmail.com
+            var atIndex = email.IndexOf('@');
+            var domain = email.Substring(email.IndexOf('@') + 1);
+
+            return domain;
+        }
+
+        static void Capitalize(ref string str)
+        {
+            str = char.ToUpper(str[0]) + str.Substring(1).ToLower();
+        }
+
+        static string[] MakeUniqueArr(string[] arr)
+        {
+            string[] newArr = new string[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Array.IndexOf(newArr, arr[i]) == -1)
+                {
+                    Array.Resize(ref newArr, newArr.Length + 1);
+                    newArr[newArr.Length - 1] = arr[i];
+                }
+            }
+
+            return newArr;
+        }
+
+        static string[] MakeNames(string[] arr)
+        {
+            string[] names = new string[arr.Length];
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                var fullname = arr[i].TrimStart();
+                var name = arr[i].TrimStart().Substring(0, fullname.IndexOf(' '));
+                names[i] = name;
+            }
+
+            return names;
+        }
+
+
+        static bool IsName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return false;
+
+            if (!char.IsUpper(name[0]))
+                return false;
+
+            for(var i=1;i<name.Length; i++)
+            {
+                if (!char.IsLower(name[i]))
+                    return false;
+            }
+            return true;
+        }
+
+
+
+    }
+}
